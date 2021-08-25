1bb2c4a9fa322155bdbf3ee7c1cd9bc01f7a2278
Adam Fravel <fravel.business@gmail.com>
2021-08-25 14:15:24 -0500

    Completed ex1b

--- 1730 AFravel 1b/frmInvoiceTotal.Designer.cs
+++ 1730 AFravel 1b/frmInvoiceTotal.Designer.cs
@@ -35,7 +35,7 @@ namespace _1730_AFravel_1b
             this.txtDiscountPercent = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.txDiscountAmt = new System.Windows.Forms.TextBox();
+            this.txtDiscountAmt = new System.Windows.Forms.TextBox();
             this.txtTotal = new System.Windows.Forms.TextBox();
             this.btnCalculate = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
@@ -44,7 +44,7 @@ namespace _1730_AFravel_1b
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 48);
+            this.label1.Location = new System.Drawing.Point(52, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 0;
@@ -53,16 +53,15 @@ namespace _1730_AFravel_1b
             // 
             // txtSubtotal
             // 
-            this.txtSubtotal.Location = new System.Drawing.Point(162, 45);
+            this.txtSubtotal.Location = new System.Drawing.Point(149, 13);
             this.txtSubtotal.Name = "txtSubtotal";
             this.txtSubtotal.Size = new System.Drawing.Size(100, 20);
             this.txtSubtotal.TabIndex = 1;
-            this.txtSubtotal.Text = " ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 86);
+            this.label2.Location = new System.Drawing.Point(52, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 13);
             this.label2.TabIndex = 2;
@@ -71,67 +70,67 @@ namespace _1730_AFravel_1b
             // 
             // txtDiscountPercent
             // 
-            this.txtDiscountPercent.Location = new System.Drawing.Point(162, 83);
+            this.txtDiscountPercent.Location = new System.Drawing.Point(149, 51);
             this.txtDiscountPercent.Name = "txtDiscountPercent";
-            this.txtDiscountPercent.ReadOnly = true;
+            this.txtDiscountPercent.ShortcutsEnabled = false;
             this.txtDiscountPercent.Size = new System.Drawing.Size(100, 20);
             this.txtDiscountPercent.TabIndex = 3;
-            this.txtDiscountPercent.TabStop = false;
+            this.txtDiscountPercent.Text = "5";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(65, 126);
+            this.label3.Location = new System.Drawing.Point(52, 94);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 13);
-            this.label3.TabIndex = 4;
+            this.label3.TabIndex = 6;
             this.label3.Text = "Discount amount:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(65, 165);
+            this.label4.Location = new System.Drawing.Point(52, 133);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
-            this.label4.TabIndex = 6;
+            this.label4.TabIndex = 8;
             this.label4.Text = "Total:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // txDiscountAmt
+            // txtDiscountAmt
             // 
-            this.txDiscountAmt.Location = new System.Drawing.Point(162, 123);
-            this.txDiscountAmt.Name = "txDiscountAmt";
-            this.txDiscountAmt.ReadOnly = true;
-            this.txDiscountAmt.Size = new System.Drawing.Size(100, 20);
-            this.txDiscountAmt.TabIndex = 5;
-            this.txDiscountAmt.TabStop = false;
+            this.txtDiscountAmt.Location = new System.Drawing.Point(149, 91);
+            this.txtDiscountAmt.Name = "txtDiscountAmt";
+            this.txtDiscountAmt.ReadOnly = true;
+            this.txtDiscountAmt.Size = new System.Drawing.Size(100, 20);
+            this.txtDiscountAmt.TabIndex = 7;
+            this.txtDiscountAmt.TabStop = false;
             // 
             // txtTotal
             // 
-            this.txtTotal.Location = new System.Drawing.Point(162, 162);
+            this.txtTotal.Location = new System.Drawing.Point(149, 130);
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
             this.txtTotal.Size = new System.Drawing.Size(100, 20);
-            this.txtTotal.TabIndex = 7;
+            this.txtTotal.TabIndex = 9;
             this.txtTotal.TabStop = false;
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(68, 200);
+            this.btnCalculate.Location = new System.Drawing.Point(55, 168);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(75, 23);
-            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.TabIndex = 4;
             this.btnCalculate.Text = "&Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
             this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(187, 200);
+            this.btnExit.Location = new System.Drawing.Point(174, 168);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
-            this.btnExit.TabIndex = 3;
+            this.btnExit.TabIndex = 5;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
@@ -141,11 +140,11 @@ namespace _1730_AFravel_1b
             this.AcceptButton = this.btnCalculate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 259);
+            this.ClientSize = new System.Drawing.Size(293, 204);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtTotal);
-            this.Controls.Add(this.txDiscountAmt);
+            this.Controls.Add(this.txtDiscountAmt);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtDiscountPercent);
@@ -167,7 +166,7 @@ namespace _1730_AFravel_1b
         private System.Windows.Forms.TextBox txtDiscountPercent;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txDiscountAmt;
+        private System.Windows.Forms.TextBox txtDiscountAmt;
         private System.Windows.Forms.TextBox txtTotal;
         private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Button btnExit;
--- 1730 AFravel 1b/frmInvoiceTotal.cs
+++ 1730 AFravel 1b/frmInvoiceTotal.cs
@@ -19,9 +19,14 @@ namespace _1730_AFravel_1b
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-          //  txtTotal.Text = "Hello";
-          //  txtTotal.ReadOnly = true;
-          //  txtTotal.TabIndex = 5;
+            //txtTotal.Text = "Hello";
+            //txtTotal.ReadOnly = true;
+            //txtTotal.TabIndex = 5;
+            //txtDiscountAmt.Text = txtDiscountPercent.Text;
+
+            txtDiscountAmt.Text = (Convert.ToDecimal(txtSubtotal.Text) * Convert.ToDecimal(txtDiscountPercent.Text) / 100).ToString("0.00");
+
+            txtTotal.Text = (Convert.ToDecimal(txtSubtotal.Text) - Convert.ToDecimal(txtDiscountAmt.Text)).ToString("0.00");
         }
 
         private void btnExit_Click(object sender, EventArgs e)
