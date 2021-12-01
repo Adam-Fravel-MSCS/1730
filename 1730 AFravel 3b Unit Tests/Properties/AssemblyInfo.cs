6f8d418e52b0d3d84a9cf3da8efd39a5197ed994
Delighted-Fun <82475986+Delighted-Fun@users.noreply.github.com>
2021-12-01 12:19:46 -0600

    Completed ex 3b

---
+++ 1730 AFravel 3b Unit Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1730 AFravel 3b Unit Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1730 AFravel 3b Unit Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6fcc36c-bb1a-4e75-9e65-7fd9f57d72e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- 1730 AFravel 3b/Ex3bCalculations.cs
+++ 1730 AFravel 3b/Ex3bCalculations.cs
@@ -8,17 +8,25 @@ namespace _1730_AFravel_3b
 {
     public class Ex3bCalculations
     {
+        // 0) Calculate discountPercent
         public static decimal GetDiscountPercent(decimal subtotal)
         {
             decimal discountPercent = 0m;
-
             if (subtotal >= 500m)
                 discountPercent = 0.2m;
             else
                 discountPercent = 0.1m;
-
             return discountPercent;
         }
+        // 1) Calculate discountPercent using output argument
+        public static void GetDiscountPercent(decimal subtotal, out decimal discountPercent)
+        {
+            if (subtotal >= 500m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+        }
+        // 2) Calculate futureValue
         public static decimal CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months)
         {
             decimal futureValue = 0m;
@@ -28,5 +36,66 @@ namespace _1730_AFravel_3b
             }
             return futureValue;
         }
+        // 3) Calculate futureValue using ref argument
+        public static void CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months, ref decimal futureValue)
+        {
+            for (int i = 1; i <= months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+        }
+        // 4) Calculate degrees Celsius
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double calculation = 0.0;
+            calculation = (fahrenheit - 32) * 5.0 / 9.0;
+            return calculation;
+        }
+        // 5) Calculate degrees Fahrenheit
+        public static void CelsiusToFahrenheit(double celsius, out double fahrenheit)
+        {
+            fahrenheit = (celsius * 9/5) + 32;
+        }
+        // 6) Gross Pay
+        public static decimal GrossPay(decimal hours, decimal rate)
+        {
+            decimal total = 0;
+            if (hours <= 40)
+            {
+                total = hours * rate;
+            }
+            else
+            {
+                decimal standardPay = 0.0m;
+                decimal overtimePay = 0.0m;
+                decimal overtimeMultiplier = 1.5m;
+                standardPay = (40 * rate);
+                overtimePay = (hours - 40) * (rate * overtimeMultiplier);
+                total = standardPay + overtimePay;
+            }
+            return total;
+        }
+        // 7) Total Hours
+        public static decimal TotalHours(string strNumbers)
+        {
+            decimal total = 0;
+            int startIndex = 0;
+            while (startIndex < strNumbers.LastIndexOf(' '))
+            {
+                int endIndex = strNumbers.IndexOf(' ', startIndex);
+                string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                decimal number = Decimal.Parse(strNumber);
+                total += number;
+                startIndex = endIndex + 1;
+            }
+            return total;
+        }
+        // 8) Gross Pay (string)
+        public static decimal GrossPay(string strNumbers, decimal rate)
+        {
+            decimal totalHours = TotalHours(strNumbers);
+            return GrossPay(totalHours, rate);
+        }
     }
 }
+
