8221b73031b4fda95a0a958e3e2b2cf5c67c5a3e
Delighted-Fun <82475986+Delighted-Fun@users.noreply.github.com>
2021-12-13 20:04:31 -0600

    Completed ex 3c

---
+++ 1730 AFravel 3c Unit Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1730 AFravel 3c Unit Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1730 AFravel 3c Unit Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50935bf7-d364-4b33-96e2-2047051f8652")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 1730 AFravel 3c/Ex3cCalculations.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1730_AFravel_3c
+{
+    public class Ex3cCalculations
+    {
+        // Calc 0
+        public static string Calc0(int index)
+        {
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            if (index > 0 == index < 8)
+            {
+                for (int i = 0; i < days.Length;)
+                {
+                    return days[index - 1];
+                }
+            }
+            else
+            {
+                return "Invalid index";
+            }
+            return "Invalid index";
+        }
+        // Calc 1
+        public static string Calc1(string search)
+        {
+            string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+            string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
+            search = search.Trim().ToUpper();
+            int index = Array.IndexOf(days, search);
+            if (index >= 0)
+            {
+                return hours[index];
+            }
+            else
+            {
+                return "Invalid input";
+            }
+        }
+        // Calc 2
+        public static int Calc2(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int ok in numbers)
+            {
+                sum += ok;
+            }
+            return sum;
+        }
+        // Calc 3
+        public static double Calc3(double[] numbers,  int count)
+        {
+            double sum = 0.0;
+            if (numbers.GetLength(0) > 1)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    sum += numbers[i];
+                }
+                return sum;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+        // Calc 5
+        public static double Calc5(double[] numbers)
+        {
+            if (numbers.GetLength(0) > 0)
+            {
+                int count = Int32.Parse(numbers.GetLength(0).ToString());
+                double sum = Ex3cCalculations.Calc3(numbers, count);
+                double average = sum / count;
+                return average;
+            }
+            return 0.0;
+        }
+        //Calc 6
+        public static double[] Calc6(double[] numbers)
+        {
+            int length = numbers.GetLength(0);
+            List<double> aboveAvgList = new List<double>();
+            if (length > 0)
+            {
+                double avg = Calc5(numbers);
+                foreach (int number in numbers)
+                {
+                    if (number >= avg)
+                    {
+                        aboveAvgList.Add(number);
+                    }
+                }
+            }
+            return aboveAvgList.ToArray();
+        }
+    }
+}
